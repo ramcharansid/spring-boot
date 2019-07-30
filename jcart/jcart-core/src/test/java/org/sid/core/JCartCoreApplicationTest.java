a84d94db110b83bd3e552f00108503ea81a71059
siddharth <charansuperstud@gmail.com>
2019-07-30 13:33:58 +0530

    Created project structure

---
+++ jcart/jcart-core/src/test/java/org/sid/core/JCartCoreApplicationTest.java
@@ -0,0 +1,26 @@
+package org.sid.core;
+
+import static org.junit.Assert.assertEquals;
+
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = JCartCoreApplication.class)
+public class JCartCoreApplicationTest {
+	@Autowired
+	DataSource dataSource;
+
+	@Test
+	public void testDummy() throws SQLException {
+		String schema = dataSource.getConnection().getCatalog();
+		assertEquals("jcart", schema);
+	}
+}
